3a1e9296c5e35e42ed58f68802084e548977683c
David Fisher <tibbon@gmail.com>
2014-01-22 10:50:52 -0500

    Almost done

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,10 +1,25 @@
 class PostController < ApplicationController
   def create
+  	binding.pry
+  	@post = Post.new(request_data: params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @post }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def index
+  	@posts = Post.all
   end
+
 end
